23ec5f1e3c0629d29f1ae1aa387bb2aa16bf9ec6
javedmemon1950 <49835558+javedmemon1950@users.noreply.github.com>
2019-04-22 10:13:58 +0500

    Addition and subtraction

--- main.c
+++ main.c
@@ -1,3 +1,43 @@
-void main(){
-printf("sagheer")
+void main()
+{
+	int val1,val2,result;
+	int choice;
+	clrscr();
+	printf("*********Calculator**********");
+	printf("\n1) Addition");
+	printf("\n2) Subtraction");
+	printf("\n3) Multiplication");
+	printf("\n4) Division");
+	printf("\n5) Exit");
+	printf("\nSelect any choice:");
+	scanf("%d",&choice);
+
+	switch (choice){
+
+	case 1:
+	{
+		printf("Enter first value:");
+		scanf("%d",&val1);
+		printf("Enter second value:");
+		scanf("%d",&val2);
+		result=val1+val2;
+		printf("Addition = %d",result);
+	}
+	case 2:
+	{
+		printf("enter first value :");
+		scanf("%d",&val1);
+		printf("enter second value:");
+		scanf("%d",&val2);
+		result=val1-val2;
+		printf("subtraction =%d",result);
+	}
+	default:
+	{
+		printf("Invalid...!");
+	}
+
+	}
+
+	getch();
 }
